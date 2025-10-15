445532d233b7cfa9bc5737548573bb74e0f42786
fefen <felipenaraki@gmail.com>
2025-10-14 21:48:16 -0300

    pagina de login,cadastro e homepage dentro de apps/app_cadastro

--- pirotech/apps/app_cadastro/views.py
+++ pirotech/apps/app_cadastro/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def login(request):
+    return render(request,'app_cadastro/login.html')
+
+def cadastro(request):
+    return render(request,'app_cadastro/cadastro.html')
+
+def cadastro_home(request):
+    return render(request,'app_cadastro/home.html')
